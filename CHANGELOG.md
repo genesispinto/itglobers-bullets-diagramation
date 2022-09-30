1111e06415af58300f58fe8d81e176014b4c5f02
genesis pinto <genesisgps@gmail.com>
2022-09-30 19:04:07 -0300

    feat: continuacion desarrollo app BulletsGroup

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,5 @@
 # Changelog
-Se crea app para boton de whatsapp, el cual enviara un mensaje predeterminado a un numero de empresa para la comunicacion entre cliente y tienda
+App bullets-diagramation:  Contexto de elementos que renderizaran los niveles inferiores de navegacion dentro de una lista de bullets con imagenes
 
 ## [Unreleased]
 
--- react/components/BulletGroup/index.tsx
+++ react/components/BulletGroup/index.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, {PropsWithChildren} from 'react'
+import {useListContext} from 'vtex.list.context'
+import { BulletsSchema } from './BulletTypes'
+import {useDevice} from 'vtex.device-detector'
 
-const BulletGroup =()=>{
+export interface BulletGroupProps {
+    bullets: BulletsSchema
+}
+
+const BulletGroup =({
+    bullets,
+    children
+}: PropsWithChildren<BulletGroupProps>) =>{
+    const {isMobile}= useDevice();
+    const {list} = useListContext() || []
+    console.log("bullets",list)
+    if(false){
+        console.log(children,bullets)
+    } 
     return(
-    <div> Aqui tendremos un list context</div>
-        
+          isMobile
+        ?
+             <div> Estamos en mobile</div>
+        :
+            <div> Estamos en desktop</div>
     )
 }
 export default BulletGroup;
