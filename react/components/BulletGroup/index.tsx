bc4f6320101c4a667472632e20c068fed918dab3
genesis pinto <genesisgps@gmail.com>
2022-10-03 17:34:20 -0300

    feat: list context desktop

--- react/components/BulletGroup/index.tsx
+++ react/components/BulletGroup/index.tsx
@@ -1,28 +1,36 @@
-import React, {PropsWithChildren} from 'react'
-import {useListContext} from 'vtex.list.context'
+import React, { PropsWithChildren } from 'react'
+import { useListContext, ListContextProvider } from 'vtex.list-context'
 import { BulletsSchema } from './BulletTypes'
-import {useDevice} from 'vtex.device-detector'
+import { useDevice } from 'vtex.device-detector'
+import getBulletsAsTSXlist from './modules/bulletsAsList'
+
 
 export interface BulletGroupProps {
     bullets: BulletsSchema
 }
 
-const BulletGroup =({
+const BulletGroup = ({
     bullets,
     children
-}: PropsWithChildren<BulletGroupProps>) =>{
-    const {isMobile}= useDevice();
-    const {list} = useListContext() || []
-    console.log("bullets",list)
-    if(false){
-        console.log(children,bullets)
-    } 
-    return(
-          isMobile
-        ?
-             <div> Estamos en mobile</div>
-        :
-            <div> Estamos en desktop</div>
+}: PropsWithChildren<BulletGroupProps>) => {
+    const { isMobile } = useDevice();
+    const { list } = useListContext() || []
+
+    console.log("bullets", bullets)
+
+    const bulletsGroup = getBulletsAsTSXlist(bullets)
+    const newListContextValue = list.concat(bulletsGroup)
+
+
+    return (
+        <ListContextProvider list={newListContextValue}>
+            {isMobile
+                ?
+                <div> {bulletsGroup}</div>
+                :
+                children
+            }
+        </ListContextProvider>
     )
 }
 export default BulletGroup;
